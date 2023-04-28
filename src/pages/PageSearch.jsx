b85811e200d27ac8a84bfd9e4e867f551b56e684
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-28 10:57:13 +0200

    pages created and cp script

---
+++ src/pages/PageSearch.jsx
@@ -0,0 +1,7 @@
+export const PageSearch = () => {
+	return (
+		<div className="page pageSearch">
+			<p>This is the Search page.</p>
+		</div>
+	);
+};
---
+++ src/pages/PageWelcome.jsx
@@ -0,0 +1,7 @@
+export const PageWelcome = () => {
+	return (
+		<div className="page pageWelcome">
+			<p>This is the Welcome page.</p>
+		</div>
+	);
+};
